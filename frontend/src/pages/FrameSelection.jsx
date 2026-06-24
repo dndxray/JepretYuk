3f98f62d52b27d180633a0be3a94bf5b2d7ff9d2
Dinda Isyariani <isyarianid2@g,ail.com>
2026-06-24 23:03:37 +0700

    fix codebase

--- frontend/src/pages/FrameSelection.jsx
+++ frontend/src/pages/FrameSelection.jsx
@@ -10,38 +10,22 @@ function FrameSelection() {
         localStorage.setItem("selectedFrame", frame);
         navigate("/camera");
     };
+    return (
+        <div className="frame-page">
+            <h1>Choose Your Frame</h1>
+            <p>Select your favorite photobooth frame</p>
 
-  return (
-    <div className="frame-page">
-        <h1>Choose Your Frame</h1>
-        <p>Select your favorite photobooth frame</p>
-        <div className="frame-grid">
-        <div
-            className="frame-card"
-            onClick={() =>
-                selectFrame("framewonyoung")
-            }
-        >
-            <img
-                src={framewonyoung}
-                alt="Wonyoung Frame"
-            />
-        </div>
+            <div className="frame-grid">
+                <div className="frame-card" onClick={() => selectFrame("framewonyoung")}>
+                    <img src={framewonyoung} alt="Wonyoung Frame" />
+                </div>
 
-        <div
-            className="frame-card"
-            onClick={() =>
-                selectFrame("framekarina")
-            }
-        >
-            <img
-                src={framekarina}
-                alt="Karina Frame"
-            />
+                <div className="frame-card" onClick={() => selectFrame("framekarina")}>
+                    <img src={framekarina} alt="Karina Frame" />
+                </div>
+            </div>
         </div>
-        </div>
-    </div>
-  );
+    );
 }
 
 export default FrameSelection;
